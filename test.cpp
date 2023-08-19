b102bc1988b3ab74f856450e30fc0f5c672cf3db
Bennett Wiredu <bwiredu@cs.ualberta.ca>
2023-08-19 15:17:06 -0600

    X

--- test.cpp
+++ test.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(){
 
 	cout << "I am talking to you, remote server." << endl;
+	cout << "Here is remote! Calling from remote branch. Going to commit this change and merge 2 master."
 	return 0;
 }
